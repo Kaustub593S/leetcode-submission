29bae4cd472ba52da1214937108d2da4fc8bf198
Kaustub Gairola <mailtokaustubgairola@gmail.com>
2026-07-08 22:14:12 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (84.73%) - LeetSync

---
+++ 941-sort-array-by-parity/sort-array-by-parity.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int slow=0;
+        int fast=0;
+
+        while(fast<nums.length)
+        {
+            if(nums[fast]%2==0)
+            {
+                int temp=nums[slow];
+                nums[slow]=nums[fast];
+                nums[fast]=temp;
+                slow++;
+            }
+            fast++;
+
+        }
+        return nums;
+    }
+}
